f426b91f022f1033b882086c4e92b53f5aae194b
Pankajpreet Singh <pankajpreet86@gmail.com>
2024-05-15 00:20:09 -0600

    assignment complete

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const app = Vue.createApp(
+const app = Vue.createApp({
     data(){
-
+        return {
+            person: {
+                name:'Pankajpreet Singh',
+                age: 38,
+                avatar: 'https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg'
+            },
+        }
     },
-    methods: {
-
+    methods:{
+        ageInFiveYears(){
+            return this.person.age + 5
+        },
+        randomNumber(){
+            return Math.random()
+        }
     }
-
-);
+});
 
 app.mount('#assignment')
