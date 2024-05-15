14b7d439333a653b1348716f5b50ae170114478c
Pankajpreet Singh <pankajpreet86@gmail.com>
2024-05-14 21:05:21 -0600

    first draft

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-Vue.createApp({
-    data() {
-        return{
-            goals: [],
-            enteredValue: ''         
-        };
+const app = Vue.createApp(
+    data(){
+
     },
     methods: {
-        addGoal(){
-            this.goals.push(this.enteredValue)
-            this.enteredValue = ''
-        }
+
     }
-}).mount('#app')
 
+);
 
-// console.log('Hello')
-// const textElement = document.getElementById('goal')! as HTMLInputElement
-// const submitButton = document.querySelector('button')! as HTMLButtonElement
-// const listElement = document.querySelector('ul')! as HTMLUListElement
-// submitButton.addEventListener('click', (event: Event)=>{
-//     console.log(textElement.value)
-//     const listItem = document.createElement('li')
-//     listItem.textContent = textElement.value
-//     listElement.appendChild(listItem)
-//     textElement.value = ""
-// })
+app.mount('#assignment')
