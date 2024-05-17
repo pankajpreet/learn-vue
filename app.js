bb783eb56c3b0d31e49d041d13e6a98f5f3cd4d0
Pankajpreet Singh <pankajpreet86@gmail.com>
2024-05-16 18:57:28 -0600

    done

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-Vue.createApp({
-    data() {
-        return{
-            goals: [],
-            enteredValue: ''         
-        };
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "John Doe"
+    };
+  },
+  methods:{
+    incrementCounter(num){
+      this.counter = this.counter + num
     },
-    methods: {
-        addGoal(){
-            this.goals.push(this.enteredValue)
-            this.enteredValue = ''
-        }
+    decrementCounter(){
+      this.counter = this.counter - 1
+    },
+    setName(event, lastName){
+      this.name = event.target.value  + lastName
+    },
+    submitForm(event){      
+      alert('submitted')
     }
-}).mount('#app')
 
 
-// console.log('Hello')
-// const textElement = document.getElementById('goal')! as HTMLInputElement
-// const submitButton = document.querySelector('button')! as HTMLButtonElement
-// const listElement = document.querySelector('ul')! as HTMLUListElement
-// submitButton.addEventListener('click', (event: Event)=>{
-//     console.log(textElement.value)
-//     const listItem = document.createElement('li')
-//     listItem.textContent = textElement.value
-//     listElement.appendChild(listItem)
-//     textElement.value = ""
-// })
+  },
+});
+
+app.mount('#events');
