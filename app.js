89282b2ae4c2c449c9a5ad1a678ce7c1eedbce9d
Pankajpreet Singh <pankajpreet86@gmail.com>
2024-05-20 13:51:09 -0600

    assignment 3 completed

--- app.js
+++ app.js
@@ -1,48 +1,35 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: ''
-    };
-  },
-  watch: {
-    counter(value){
-      if (value > 100){
-        this.counter = 0;
-      }
+    data(){
+       return {
+            result: 0
+        }
+    },
+    methods:{
+        add(num){
+            this.result = this.result + num
+        }
+    },
+    computed:{
+        printResult(){
+            if (this.result < 37){
+                return 'not there yet!!'
+            } else if(this.result > 37){
+                return 'too much'
+            } else{
+                return '37'
+            }
+        }
+    },
+    watch:{
+        result(newValue, oldValue){
+            that = this
+            if(newValue > 37){
+                setTimeout(() => {
+                    that.result = 0;
+                  }, "5000");
+            }
+        }
     }
-  },
-  computed: {
-    fullName(){
-
-      if(this.name ==''){
-        return '';
-      }
-      return this.name + ' ' + 'LAST'
-
-    }
-  },
-  methods: {
-    outputFullName(){
-      if(this.name ==''){
-        return '';
-      }
-      return this.name + ' ' + 'LAST'
-    },
-    setName(event, lastName) {
-      this.name = event.target.value;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    },
-    resetInput(){
-      this.name = '';
-    }
-  }
 });
 
-app.mount('#events');
+app.mount('#assignment')
