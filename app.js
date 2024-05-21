db22f765052df619328ca1732985553442498620
Pankajpreet Singh <pankajpreet86@gmail.com>
2024-05-20 18:18:36 -0600

    assignment completed

--- app.js
+++ app.js
@@ -1,48 +1,22 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: ''
-    };
-  },
-  watch: {
-    counter(value){
-      if (value > 100){
-        this.counter = 0;
-      }
+const app = Vue.createApp(
+    {
+        data(){
+            return {
+                tasks: [],
+                enteredTask: '',
+                isVisible: true
+            }
+        },
+        methods:{
+            addTask(){
+                this.tasks.push(this.enteredTask)
+                this.enteredTask = ''
+            },
+            toggleList(){
+                this.isVisible = !this.isVisible
+            }
+        }
     }
-  },
-  computed: {
-    fullName(){
+)
 
-      if(this.name ==''){
-        return '';
-      }
-      return this.name + ' ' + 'LAST'
-
-    }
-  },
-  methods: {
-    outputFullName(){
-      if(this.name ==''){
-        return '';
-      }
-      return this.name + ' ' + 'LAST'
-    },
-    setName(event, lastName) {
-      this.name = event.target.value;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    },
-    resetInput(){
-      this.name = '';
-    }
-  }
-});
-
-app.mount('#events');
+app.mount('#assignment')
